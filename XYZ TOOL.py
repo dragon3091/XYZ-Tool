50679390443ed614ebbae3461a1cab7f392eda22
dragon3091 <89909316+dragon3091@users.noreply.github.com>
2021-09-07 22:57:09 +0200

    Update XYZ TOOL.py

--- XYZ TOOL.py
+++ XYZ TOOL.py
@@ -7,90 +7,27 @@ def slowtype(t):
 os.system('clear')
 ############
 print('\033[33m')
-print("""
+slowtype("""
 __  ___   _______  _____ ___   ___  _     
 \ \/ \ \ / |__  / |_   _/ _ \ / _ \| |    
  \  / \ V /  / /    | || | | | | | | |    
  /  \  | |  / /_    | || |_| | |_| | |___ 
 /_/\_\ |_| /____|   |_| \___/ \___/|_____|""")
-slowtype("____________________________________________")
+slowtype("_____________________________")
 slowtype ("\033[33;1mWELCOM TO V1 OF XYZ TOOL")
-print ("______________________________")
+print ("__________________")
 print("you can download important tools here")
 print("             ")
 print("\033[36;1m1-Metasploit\n2-OSIF\n3-T-Header\n4-Hack cam\n")
 k1 = input ("what do you want: ")
 print ("               ")
-if k1 == "1" :
-    print("""\033[32mpkg update && pkg upgrade - y
-
-pkg insatll wget
-
-pkg install ruby
-
-wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
-
-./metasploit.sh
-
-msfconsol""")
+if k1 == "1" :  
+ os.system('pkg update && pkg upgrade - y && pkg insatll wget -y && pkg install ruby -y && wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh')
 if k1== "2":
-    print ("""pkg update
-
-pkg upgrade
-
-pkg install python
-
-pkg install python2
-
-pkg install git
-
-git clone https://github.com/CiKu370/0SIF
-Is
-
-cd OSIF
-
-Is
-
-chmod +x *
-
-Is
-pip2 install -r requirements.txt
-
-Is
-cd osif.py
-
-python2 osif.py""")
+   os.system('pkg update&&pkg upgrade&&pkginstall python&&pkg install python2&&pkg install git&&git clone https://github.com/CiKu370/0SI F&&cd OSIF&&chmod +x *&&pip2 install -r requirements.txt&&cd osif.py&&python2 osif.py')
 if k1== "3" : 
-    print ("""to Type Your Name in termux 
-
-apt update && apt upgrade
-
-
-
- git clone https://github.com/remo7777/T-Header
-
-
- cd T-Header
-
-
- chmod 777 t-header.sh
-
-
- bash t-header.sh
-
-
-Type Your Name and yes""")
+    os.system("apt update && apt upgrade&&git clone https://github.com/remo7777/T-Header&&cd T-Header&&chmod 777 t-header.sh&&bash t-header.sh&&Type Your Name and yes")
 if k1== "4" :
-	print("""pkg update
-  pkg upgrade
-    pkg install python2
-   pkg install python
-
-    pip2 install requests
-    pkg install git
-   git clone https://github.com/kancotdiq/ipcs
-   cd ipcs
-   ls
-    python2 scan.py""")
+	os.systeem('pkg update&&pkg upgrade&&pkg install python2&&pkg install python&&pip2 install requests&&pkg install git&&git clone https://github.com/kancotdiq/ipcs&&cd ipcs&&python2 scan.py')
 else : 
     os.system('exit')
